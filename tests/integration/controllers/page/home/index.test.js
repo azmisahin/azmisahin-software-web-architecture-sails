a0aeb0055229378d19443d57e9120fd1e2fba033
Azmi SAHIN <azmisahin@outlook.com>
2020-05-25 13:13:37 +0300

    Integration

---
+++ tests/integration/controllers/page/home/index.test.js
@@ -0,0 +1,8 @@
+describe('home.index', function () {
+  describe('Body', function () {
+    it('the main body should be loaded.', function (done) {
+      //supertest(app).get('/').expect(200).end(done)
+      done()
+    })
+  })
+})
