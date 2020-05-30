4f29aeafc66e56437e09b2bcfd713953ca5953f4
Azmi SAHIN <azmisahin@outlook.com>
2020-05-30 19:53:27 +0300

    #12 product enviroment configured

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,10 @@
 module.exports = {
   datastores: {
     default: {
-      // adapter: process.env.CONNECTION_STRING == "" ? "" : "sails-mongo",
-      // url: process.env.CONNECTION_STRING,
+      adapter:
+        process.env['CONNECTION_STRING'] === undefined
+          ? 'sails-disk'
+          : 'sails-mongo',
     },
   },
   session: {
@@ -12,8 +14,13 @@ module.exports = {
     cookie: {
       maxAge: 1 * 60 * 60 * 1000, // 1 hours
     },
-    // adapter: process.env.CONNECTION_STRING == "" ? "" : "connect-mongo",
-    // url: process.env.CONNECTION_STRING,
+    /**
+     * MongoDB session store
+     * adapter version required : "connect-mongo": "2.0.3"
+     * adapter version ERROR    : "connect-mongo": "^3.2.0",
+     */
+    adapter: process.env.CONNECTION_STRING == '' ? '' : 'connect-mongo',
+    url: process.env.CONNECTION_STRING,
   },
   models: {
     migrate: 'safe',
